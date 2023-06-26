b5c28c6b79f3fb5a16655bdd06b9f1afc1732631
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2023-06-27 01:14:45 +0530

    Add files via upload

---
+++ Lec_005/5_1.cpp
@@ -0,0 +1,42 @@
+// Diferrence of product and sum of a number 
+// num = 36
+// result = (3x6) - (3+6) = 9
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+//     int prod = 1;
+//     int sum = 0;
+//     while (n != 0) {
+//         int digit = n % 10;
+//         prod = prod * digit;
+//         sum = sum + digit;
+//         n = n / 10;
+//     }
+//     int answer = prod - sum;
+//     cout<< answer;
+// }
+
+
+
+
+// No.Of One Bits
+
+// int main() {
+//     int n;
+//     cin>>n;
+//     int count = 0;
+//     while (n != 0){
+//         // checking last bit
+//         if (n & 1){
+//             count++;
+//         }
+//         n = n >> 1;
+//     }
+//     return count;
+// }
+
+
+
