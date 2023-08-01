7631e6fc3a2173e9303d6d4e4374a0ec73bf0e2b
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2023-08-02 01:05:35 +0530

    Add files via upload

---
+++ Lec_021/21_2.cpp
@@ -0,0 +1,63 @@
+// Question Link :- https://bit.ly/3DXfsDZ 
+// Add two array (Important)
+
+vector<int> reverse(vector<int> v) {
+    int s = 0;
+    int e = v.size()-1;
+    while(s<e) {
+        swap(v[s++], v[e--]);
+    }
+    return v;
+}
+
+vector<int> findArraySum(vector<int>&a, int n, vector<int>&b, int m) {     // ques start
+    int i = n-1;
+    int j = m-1;
+    vector<int> ans;
+    int carry = 0;
+    while(i>=0 && j>=0) {
+        int val1 = a[i];
+        int val2 = b[j];    
+        int sum = val1 + val2 + carry;   
+        carry = sum/10;
+        sum = sum%10;
+        ans.push_back(sum);
+        i--;
+        j--;
+    }
+    
+    // first case
+    while(i>=0) {
+        int sum = a[i] + carry;
+        carry = sum/10;
+        sum = sum%10;
+        ans.push_back(sum);
+        i--;
+    }
+    
+     // second case
+    while(j>=0) {
+        int sum = b[j] + carry;
+        carry = sum/10;
+        sum = sum%10;
+        ans.push_back(sum);
+        j--;
+    }
+    
+     // third case
+    while(carry != 0) {
+        int sum = carry;
+        carry = sum/10;
+        sum = sum%10;
+        ans.push_back(sum);
+    }
+    
+    //print(ans);
+    return reverse(ans);
+}
+
+
+
+// first case                         second case                        third case (carry)
+//   4 5 6 9                                   6                          5 8 9 7
+//         8                           4 3 6 5 9                          9 4 1 3
