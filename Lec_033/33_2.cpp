9a7f3852801ec21996bd53f6fcd3ef4016434efe
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2023-09-04 22:55:28 +0530

    Add files via upload

---
+++ Lec_033/33_2.cpp
@@ -0,0 +1,56 @@
+// Find whether given no. is present in array or not using linear search in recursion.
+
+#include<iostream>
+using namespace std;
+
+void print(int arr[], int n) {
+    cout << "Size of array is : " << n << endl;
+    cout << "Array : ";
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    } cout << endl;
+
+}
+
+bool linearSearch(int arr[], int size, int k ) {
+    print(arr,size);
+    //base case
+    if(size == 0) 
+        return false;
+
+    if(arr[0] == k) {
+        return true;
+    }
+    else {
+        bool remainingPart = linearSearch(arr+1, size-1, k );
+        return remainingPart;
+    }
+}
+
+int main() {
+    int arr[5] = {3,5,1,2,6};
+    int size = 5;
+    int key = 2;
+    bool ans = linearSearch(arr, size, key);
+    if(ans ){
+        cout << "Present " << endl;
+    }
+    else{
+        cout << "absent " << endl;
+    }
+
+    return 0;
+}
+
+
+// Output :-
+
+// Size of array is : 5
+// Array : 3 5 1 2 6
+// Size of array is : 4
+// Array : 5 1 2 6
+// Size of array is : 3
+// Array : 1 2 6
+// Size of array is : 2
+// Array : 2 6 
+// Present
---
+++ Lec_033/33_3.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+void print(int arr[], int s, int e) {
+    for(int i=s; i<=e; i++) {
+        cout << arr[i] << " ";
+    } cout << endl;
+}
+
+bool binarySearch(int *arr, int s, int e , int k ) {
+    cout << endl;
+    print(arr, s, e);
+    //base case
+
+    //element not found
+    if(s>e)
+        return false;
+
+    int mid = s + (e-s)/2;
+    cout << "Value of mid is : " << arr[mid] << endl;
+    //element found
+    if(arr[mid] == k)
+        return true;
+
+    if(arr[mid] < k) {
+        return binarySearch(arr, mid+1, e, k);
+    }
+    else{
+        return binarySearch(arr, s, mid-1, k);
+    }
+}
+
+
+int main() {
+    int arr[11] = {2,4,6,10,14,18,22,38,49,55,224};
+    int size = 11;
+    int key = 222;
+    cout << "Present or not : " << binarySearch(arr, 0, size-1, key) << endl;
+
+    return 0;
+}
+
+
+
+// Output :-
+
+// 2 4 6 10 14 18 22 38 49 55 224
+// Value of mid is : 18
+
+// 22 38 49 55 224
+// Value of mid is : 49
+
+// 55 224
+// Value of mid is : 55
+
+// 224
+// Value of mid is : 224
+
+
+// Present or not : 0
