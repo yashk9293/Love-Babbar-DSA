2f450e47a4380e8dab8bb44cc51569aeef81cff1
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2023-09-10 11:27:02 +0530

    Add files via upload

---
+++ Lec_043/43_3.cpp
@@ -0,0 +1,32 @@
+// Single Inheritance
+
+#include<iostream>
+using namespace std;
+
+class Animal {
+    public:
+    int age;
+    int weight;
+
+    public:
+    void speak() {
+        cout << "Speaking " << endl;
+    }
+};
+
+class Dog: public Animal {
+
+};
+
+
+int main() {
+    Dog d;
+    d.speak();
+    cout << d.age << endl;
+
+    return 0;
+}
+
+
+// Speaking 
+// 6422356 (garbage value)
