50ed58380fffcce5218719885ad928f84b2b7f37
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2023-09-10 11:28:09 +0530

    Add files via upload

---
+++ Lec_043/43_7.cpp
@@ -0,0 +1,37 @@
+// Ambiguity
+
+#include<iostream>
+using namespace std;
+
+class A {
+    public:
+
+    void func() {
+        cout << " I am A" << endl;
+    }
+};
+
+class B {
+    public:
+    void func() {
+        cout << " I am B" << endl;
+    }
+};
+
+class C: public A, public B {
+
+};
+
+int main() {
+    C obj;
+    //obj.func();
+
+    obj.A::func() ;
+    obj.B::func();
+
+    return 0;
+}
+
+
+// I am A
+// I am B
