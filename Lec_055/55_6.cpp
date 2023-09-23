250bd7910750820f816ddef069276353efac25c0
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2023-09-23 21:09:26 +0530

    Add files via upload

---
+++ Lec_055/55_6.cpp
@@ -0,0 +1,33 @@
+// Question Link :- https://www.codingninjas.com/studio/problems/redundant-brackets_975473
+// Redundant Brackets
+
+#include <bits/stdc++.h> 
+bool findRedundantBrackets(string &s) {
+    stack<char> st;
+    for(int i=0; i<s.length(); i++) {
+        char ch = s[i];
+        if(ch == '(' || ch == '+' ||ch == '-' || ch == '*' || ch == '/') {
+            st.push(ch);
+        }
+        else {
+            if(ch == ')') {
+                bool isRedundant = true;
+
+                while(st.top() != '(') {
+                    char top = st.top();
+                    if(top == '+' || top == '-' || top == '*' || top == '/') {
+                        isRedundant = false;
+                    }
+                    st.pop();
+                }
+
+                if(isRedundant == true) {
+                    return true;
+                }
+
+                st.pop();
+            }
+        }
+    }
+    return false;
+}
