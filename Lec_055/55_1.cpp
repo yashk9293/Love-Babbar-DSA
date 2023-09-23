efb540334ab71b0ec8744f6aee69fdc1078f2664
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2023-09-23 21:07:55 +0530

    Add files via upload

---
+++ Lec_055/55_1.cpp
@@ -0,0 +1,22 @@
+// Question Link :- https://www.codingninjas.com/studio/problems/delete-middle-element-from-stack_985246
+// Delete middle element from stack
+
+#include <bits/stdc++.h>
+
+void solve(stack<int> &s, int count, int size) {
+   if(count == size/2) {
+      s.pop();
+      return;
+   }
+   int num = s.top();
+   s.pop();
+
+   solve(s, count + 1, size);
+
+   s.push(num);
+}
+
+void deleteMiddle(stack<int>&inputStack, int N){
+	int count = 0;
+    solve(inputStack, count, N);
+}
