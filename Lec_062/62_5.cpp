4abc054e54325151caf541bd9990d9354821fe61
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2023-10-15 18:04:38 +0530

    Add files via upload

---
+++ Lec_062/62_5.cpp
@@ -0,0 +1,26 @@
+// Question Link :- https://leetcode.com/problems/binary-tree-preorder-traversal
+// Iterative Preorder Traversal
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preorder;
+        if(root==NULL){
+            return preorder;
+        }
+        stack<TreeNode *> st;
+        st.push(root);
+        while(!st.empty()){
+            root=st.top();
+            st.pop();
+            preorder.push_back(root->val);
+            if(root->right){
+                st.push(root->right);
+            }
+            if(root->left){
+                st.push(root->left);
+            }
+        }
+        return preorder;
+    }
+};
