7264129739c3feab796dcae28c2f0b8327ede383
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2023-10-18 23:42:37 +0530

    Add files via upload

---
+++ Lec_062/62_8.cpp
@@ -0,0 +1,39 @@
+// Question Link :- https://leetcode.com/problems/binary-tree-postorder-traversal
+// Iterative Postorder Traversal using 1 stack
+
+// S.C = O(2N)
+// T.C = O(N)
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> postorder;
+        TreeNode* curr = root;
+        TreeNode* temp;
+
+        if(root == NULL) return postorder;
+
+        while(curr != NULL || !st.empty()) {
+            if(curr != NULL) {
+                st.push(curr);
+                curr = curr->left;
+            }
+            else {
+                temp = st.top()->right;
+                if(temp == NULL) {
+                    temp = st.top();
+                    st.pop();
+                    postorder.push_back(temp->val);
+                    while(!st.empty() && temp == st.top()->right) {
+                        temp = st.top();
+                        st.pop();
+                        postorder.push_back(temp->val);
+                    }
+                }
+                else curr = temp;
+            }
+        }
+        return postorder;
+    }
+};
