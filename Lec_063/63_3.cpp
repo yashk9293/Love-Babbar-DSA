b919093e97712d68a0c5f7b6feb275979a4193f0
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2023-11-29 23:16:39 +0530

    Add files via upload

---
+++ Lec_063/63_3.cpp
@@ -0,0 +1,30 @@
+// Question Link :- https://practice.geeksforgeeks.org/problems/determine-if-two-trees-are-identical/1
+// Determine if Two Trees are Identical
+
+// T.C = O(N)
+// S.C = O(N)
+class Solution {
+    public:
+    bool isIdentical(Node *r1, Node *r2) {
+        if(r1 == NULL && r2 == NULL) {
+            return true;
+        }
+        if(r1 != NULL && r2 == NULL) {
+            return false;
+        }
+        if(r1 == NULL && r2 != NULL) {
+            return false;
+        }
+        
+        bool left = isIdentical(r1->left, r2->left);
+        bool right = isIdentical(r1->right, r2->right);
+        bool value = r1->data == r2-> data;
+        
+        if(left && right && value) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
