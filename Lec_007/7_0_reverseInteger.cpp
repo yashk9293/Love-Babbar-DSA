a9fb21773ee1794e7a33141197d0396aadfdd64e
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-01-17 22:45:53 +0530

    Update 7_0_reverseInteger.cpp

--- Lec_007/7_0_reverseInteger.cpp
+++ Lec_007/7_0_reverseInteger.cpp
@@ -1,5 +1,5 @@
 /* Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value 
-to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0. */
+to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0. */
 
 
 // digit = 123
