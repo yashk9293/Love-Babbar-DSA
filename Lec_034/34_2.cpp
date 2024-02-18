b7dc698e5fd35a5c4e682ef4b99549e0b4b3fdcf
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-02-19 00:34:55 +0530

    Update 34_2.cpp

--- Lec_034/34_2.cpp
+++ Lec_034/34_2.cpp
@@ -17,3 +17,45 @@ string reverseString(string str) {
 	reverse(str, 0, str.length()-1);
 	return str;
 }
+
+
+
+
+
+
+
+// Question Link - https://leetcode.com/problems/reverse-string/
+
+// Using Recursion
+class Solution {
+public:
+    void reverse(vector<char> &s, int i, int j) {
+        if(i>j) {
+            return;
+        }
+        swap(s[i], s[j]);
+        i++;
+        j--;
+        reverse(s, i, j);
+    }
+    void reverseString(vector<char>& s) {
+        reverse(s, 0, s.size()-1);
+    }
+};
+
+
+
+
+// Best Approach is using 2 pointer
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int end=s.size()-1;
+        int start=0;
+        while(start<s.size()/2){
+            swap(s[start], s[end]);
+            start++;
+            end--;
+        }
+    }
+};
