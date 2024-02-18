bb05c6a22a116aa728204d54e48034c4e93adc12
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-02-18 19:03:14 +0530

    Update 22_6.cpp

--- Lec_022/22_6.cpp
+++ Lec_022/22_6.cpp
@@ -1,34 +1,33 @@
 // Question Link :- https://leetcode.com/problems/string-compression/   (medium)
-// String Compression
+// String Compression 
+// https://youtu.be/I7drewKcN1Y
 
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int i = 0;
-        int ansindex = 0;
         int n = chars.size();
-
+        int currChar = 0, count = 0, index = 0;
+        int i=0;
         while(i<n) {
-            int j = i+1;
-            while(j<n && chars[i]==chars[j]) {
-                j++;
+            currChar = chars[i];
+            count =0;
+            while(i<n && chars[i] == currChar) {
+                count++;
+                i++;
             }
-
-            chars[ansindex] = chars[i];
-            ansindex++;
-
-            int count = j-i;
-
-            if(count>1) {
-                string cnt = to_string(count);
-                for(char ch: cnt ) {
-                    chars[ansindex] = ch;
-                    ansindex++;
+            // assigning work
+            chars[index] = currChar;
+            index++;
+            // add the count
+            if(count > 1) {
+                string strCount = to_string(count);
+                for(auto ch : strCount) {
+                    chars[index] = ch;
+                    index++;
                 }
             }
-            i=j;
         }
-        return ansindex;
+        return index;
     }
 };
 
