c8a1c7b9c85d220d4fa17b71586d0dd2c30bc905
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-02-18 19:17:53 +0530

    Update 23_3.cpp

--- Lec_023/23_3.cpp
+++ Lec_023/23_3.cpp
@@ -5,7 +5,7 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         
-        //tranaspose
+        //transpose
         for(int i =0; i<matrix.size();i++){
             for(int j =0 ; j<i ; j++){
                 swap(matrix[i][j], matrix[j][i]);
