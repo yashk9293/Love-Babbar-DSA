5cfa7a34446a53b98794e27e74bafe132b0ee3e1
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-04-11 16:40:35 +0530

    Update 55_1.cpp

--- Lec_055/55_1.cpp
+++ Lec_055/55_1.cpp
@@ -1,6 +1,9 @@
 // Question Link :- https://www.codingninjas.com/studio/problems/delete-middle-element-from-stack_985246
 // Delete middle element from stack
 
+// Recursive Method
+// T.C = O(N)
+// S.C = O(N)
 #include <bits/stdc++.h>
 
 void solve(stack<int> &s, int count, int size) {
@@ -18,5 +21,32 @@ void solve(stack<int> &s, int count, int size) {
 
 void deleteMiddle(stack<int>&inputStack, int N){
 	int count = 0;
-    solve(inputStack, count, N);
+   solve(inputStack, count, N);
+}
+
+
+
+
+
+
+
+
+// Iterative Method
+// T.C = O(N)
+// S.C = O(N)
+void deleteMiddle(stack<int> &inputStack, int N) {
+   stack<int> st;
+   int count = 0;
+
+   while (count < N / 2) {
+      st.push(inputStack.top());
+      inputStack.pop();
+      count = count + 1;
+   }
+   inputStack.pop();
+
+   while (!st.empty()) {
+      inputStack.push(st.top());
+      st.pop();
+   }
 }
