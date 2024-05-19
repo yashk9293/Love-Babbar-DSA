325c25ec4273ef3758de2cf198c9f3293a43684e
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-05-19 20:02:43 +0530

    Update 48_2.cpp

--- Lec_048/48_2.cpp
+++ Lec_048/48_2.cpp
@@ -1,3 +1,23 @@
 // Question Link :- https://bit.ly/3rjMQC5
 // Remove duplicates from a sorted Doubly Linked List
 
+// (same code as 48_0)
+// T.C = O(n)
+// S.C = O(1)
+#include<bits/stdc++.h>
+Node * removeDuplicates(Node *head) {
+    if(head == NULL) {
+        return NULL;
+    }
+    Node* temp = head;
+    while(temp && temp->next) {  
+        if (temp->data == temp->next->data) {
+            Node *deleteNode = temp->next;    // is node ko delete karna hai
+            temp->next = temp->next->next;
+            delete deleteNode;
+        } else {
+            temp = temp->next;
+        }
+    }
+    return head; 
+}
