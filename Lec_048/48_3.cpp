6bb3d28ab31fd5abadb5851da7115b843c6c6b13
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-05-19 21:04:46 +0530

    Update 48_3.cpp

--- Lec_048/48_3.cpp
+++ Lec_048/48_3.cpp
@@ -19,3 +19,13 @@ void splitList(Node *head, Node **head1_ref, Node **head2_ref) {
     fast->next = slow->next;
     slow->next = head;
 }
+
+
+
+
+// 1 2 5 4 5 6
+// mid is : 5 and 4, but here we have to consider 5 for splitting
+// the linked list into two equal halves, isliye line 8 pe fast = head->next liye hai
+
+// warna mid nikalne ke liye slow = head aur fast = head lete hai, in 45_1
+// kyuki waha 1 2 5 4 5 6, me mid element 4 ko lete hai
