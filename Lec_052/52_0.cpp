b3a1aee24f90abb7c612f702bdb6affa15f4a9ee
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-05-20 14:07:55 +0530

    Update 52_0.cpp

--- Lec_052/52_0.cpp
+++ Lec_052/52_0.cpp
@@ -1,91 +1,9 @@
-// Question Link :- https://practice.geeksforgeeks.org/problems/clone-a-linked-list-with-next-and-random-pointer/1
-// Clone a linked list with next and random pointer
+// Question Link :- https://leetcode.com/problems/copy-list-with-random-pointer/
+// Copy List with Random Pointer
 
-
-// Approach - 1
-class Solution {
-   private:
-    void insertAtTail(Node* &head, Node* &tail, int d) {
-        Node* newNode = new Node(d);
-        if(head == NULL) {
-            head = newNode;
-            tail = newNode;
-        }
-        else {
-            tail -> next = newNode;
-            tail = newNode;
-        }
-    }
-
-    public:
-    Node *copyList(Node *head) {
-        //step 1: Create a Clone List
-        Node* cloneHead = NULL;
-        Node* cloneTail = NULL;
-        
-        Node* temp = head;
-        while(temp != NULL) {
-            insertAtTail(cloneHead, cloneTail, temp->data);
-            temp = temp -> next;
-        }
-        
-        // step 2: insert nodes of Clone List in between originalList
-        
-        Node* originalNode = head;
-        Node* cloneNode = cloneHead;
-        
-        while(originalNode != NULL && cloneNode != NULL) {
-            Node* next = originalNode -> next;
-            originalNode -> next = cloneNode;
-            originalNode = next;
-            
-            next = cloneNode -> next;
-            cloneNode -> next = originalNode;
-            cloneNode  = next;
-        }
-        
-        // step 3: Random pointer copy
-        originalNode = head;
-        cloneNode = cloneHead;
-        
-        while(originalNode != NULL && cloneNode != NULL) { 
-            
-            if(originalNode -> arb != NULL) {
-                cloneNode -> arb = originalNode -> arb -> next;
-            }
-            else {
-                cloneNode -> arb  = NULL;
-            }
-            
-            cloneNode = cloneNode -> next;
-            originalNode = originalNode -> next;
-        }
-        
-        //step 4: revert step 2 changes
-        originalNode = head;
-        cloneNode = cloneHead;
-
-        while(originalNode != NULL && cloneNode != NULL) {
-            originalNode -> next = cloneNode -> next;
-            originalNode = originalNode -> next;
-
-            if(originalNode != NULL) {
-                cloneNode -> next = originalNode -> next;
-                cloneNode = cloneNode -> next;
-            }
-        }
-
-        // step 5 answer return
-        return cloneHead;
-    }
-};
-
-
-
-
-
-
-// Approach - 2
+// Approach - 1 (Using Hashmap) [Leetcode]
+// T.C = O(n)
+// S.C (extra space) = O(n)
 class Solution {
     public:
     Node *copyList(Node *head) {
@@ -97,14 +15,60 @@ class Solution {
             mp[temp] = copy;
             temp = temp -> next;
         }
-        
         temp = head;
-        
         while(temp){
             mp[temp] -> next = mp[temp -> next];
             mp[temp] -> arb = mp[temp -> arb];
             temp = temp -> next;
         }
         return mp[head];
+    }
+};
+
+
+
+
+
+
+// Approach - 2 (codestorywithMIK)
+// T.C = O(n)
+// S.C = O(n) (because we used space in original linked list)
+// Extra Space = O(1) (because we didn't used extra space for result, and the space for original linked list is not taken into account)
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head) {
+            return NULL;
+        }
+        Node* curr = head;
+        // Adding clone nodes between the original nodes
+        while(curr) {
+            Node* currNext = curr->next;
+            curr->next = new Node(curr->val);
+            curr->next->next = currNext;
+            curr = currNext;
+        }
+        // Deep copy of random pointers
+        curr = head;
+        while(curr && curr->next) {
+            if(curr->random == NULL) {
+                curr->next->random = NULL;
+            } else {
+                curr->next->random = curr->random->next;
+            }
+            curr = curr->next->next;
+        }
+        // Recovering new copied linked list
+        Node* newHead = head->next;
+        Node* newCurr = newHead;
+        curr = head;
+        while(curr && newCurr) {
+            curr->next = curr->next ? curr->next->next : NULL;
+            newCurr->next = newCurr->next ? newCurr->next->next : NULL;
+            
+            curr = curr->next;
+            newCurr = newCurr->next;
+        }
+        return newHead;
     }
 };
