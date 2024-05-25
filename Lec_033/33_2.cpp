5a18173834201cfeefa7bd4ea6e64884febd3c47
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-05-25 15:22:11 +0530

    Update 33_2.cpp

--- Lec_033/33_2.cpp
+++ Lec_033/33_2.cpp
@@ -3,6 +3,7 @@
 #include<iostream>
 using namespace std;
 
+// Recursion from forward
 void print(int arr[], int n) {
     cout << "Size of array is : " << n << endl;
     cout << "Array : ";
@@ -15,13 +16,12 @@ void print(int arr[], int n) {
 bool linearSearch(int arr[], int size, int k ) {
     print(arr,size);
     //base case
-    if(size == 0) 
+    if(size == 0) {
         return false;
-
+    }
     if(arr[0] == k) {
         return true;
-    }
-    else {
+    } else {
         bool remainingPart = linearSearch(arr+1, size-1, k );
         return remainingPart;
     }
@@ -54,3 +54,62 @@ int main() {
 // Size of array is : 2
 // Array : 2 6 
 // Present
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Recursion from backward
+void print(int arr[], int n) {
+    cout << "Size of array is : " << n << endl;
+    cout << "Array : ";
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    } cout << endl;
+
+}
+
+bool linearSearch(int arr[], int size, int k ) {
+    print(arr,size);
+    //base case
+    if(size == 0) {
+        return false;
+    }
+    if(arr[size-1] == k) {
+        return true;
+    } else {
+        bool remainingPart = linearSearch(arr, size-1, k );
+        return remainingPart;
+    }
+}
+
+int main() {
+    int arr[5] = {3,5,1,2,6};
+    int size = 5;
+    int key = 2;
+    bool ans = linearSearch(arr, size, key);
+    if(ans ){
+        cout << "Present " << endl;
+    }
+    else{
+        cout << "absent " << endl;
+    }
+
+    return 0;
+}
+
+
+
+// Size of array is : 5
+// Array : 3 5 1 2 6 
+// Size of array is : 4
+// Array : 3 5 1 2 
+// Present 
