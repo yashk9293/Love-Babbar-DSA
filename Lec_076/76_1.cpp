2814394b677f711eb824ede1ace33bda418fcc6e
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-05-25 13:42:18 +0530

    Update 76_1.cpp

--- Lec_076/76_1.cpp
+++ Lec_076/76_1.cpp
@@ -8,100 +8,15 @@
 // sort ans array -> O(n*k log(n*k))
 
 
-// Approach - 2
-// T.C = O(n*k log(k))
-// S.C = O(k) + O(n*k) = O(n*k)
-#include <bits/stdc++.h> 
-class node {
-    public:
-    int data;
-    int i, j;
-
-    node(int data, int row, int col){
-        this->data = data;
-        this->i = row;
-        this->j = col;
-    }
-};
-
-class compare {
-    public:
-    bool operator() (node*a, node*b) {
-        return a->data > b->data;
-    }
-};
-
-vector<int> mergeKSortedArrays(vector<vector<int>>&kArrays, int k){
-    priority_queue<node*, vector<node*>, compare> minHeap;
-
-    // Step 1 : Insert all first elements in minHeap
-    for(int i=0; i<k; i++) {
-        node* temp = new node(kArrays[i][0], i, 0);
-        minHeap.push(temp);
-    }
-
-    vector<int> ans;
-
-    // Step 2 : Comparing each node and moving further in the array
-    while(minHeap.size()>0) {
-        node* temp = minHeap.top();
-        ans.push_back(temp->data);
-        minHeap.pop();
-
-        int i = temp->i;
-        int j = temp->j;
-
-        if(j+1 < kArrays[i].size()) {
-            node* next = new node(kArrays[i][j+1], i, j+1);
-            minHeap.push(next);
-        }
-    }
-    return ans;
-}
 
 
 
-// Approach - 2 (different style)
-#include<queue>
-vector<int> mergeKSortedArrays(vector<vector<int>>&kArrays, int k) {
-    vector<int> result; 
-    // Create a min heap to store atmost k heap nodes at a time.
-    priority_queue<pair<int, pair<int, int> >, vector<pair<int, pair<int, int> > >, greater<pair<int, pair<int, int> > > > minHeap; 
-  
-    for (int i = 0; i < kArrays.size(); i++) {
-        minHeap.push({ kArrays[i][0], { i, 0 } }); 
-    }
-    while (minHeap.empty() == false) { 
-        // Remove the minimum element from the heap.
-        pair<int, pair<int, int> > curr = minHeap.top(); 
-        minHeap.pop(); 
-  
-        // i is the array number and j is the index of the removed element in the ith array.
-        int i = curr.second.first; 
-        int j = curr.second.second; 
-    
-        // Add the removed element to the output array.
-        result.push_back(curr.first); 
-  
-        // If the next element of the extracted element exists, add it to the heap.
-        if (j + 1 < kArrays[i].size()) {
-            minHeap.push({ kArrays[i][j + 1], { i, j + 1 } }); 
-        }
-    }
-    return result; 
-}
-
-
-
-
-
-
-
-
-
-// Approach -  3
+// Approach -  2 (Better Approach)
+// Divide and Conquer using two-pointer technique of merging two sorted arrays
 // T.C = O((N*K) log(K))
 // S.C = O((N*K) log(K))
+// There are log (K) levels because in each level the ‘K’ arrays are divided into half. And O(N * K) space is required to store the ans. 
+
 vector<int> merge(vector<int>&a, vector<int>&b) {
     vector<int>ans; 
     int n = a.size(), m = b.size();
@@ -128,25 +43,60 @@ vector<int> merge(vector<int>&a, vector<int>&b) {
 }
 
 vector<int>  mergeKSortedArraysHelper(vector<vector<int>>&kArrays, int start, int end){
-    // If there is only one array.
-    if(start == end){
+    if(start == end) {  // If there is only one array.
         return kArrays[start];
     }
-    // If there are only two arrays, merge them.
-    if(start + 1 == end) {
+    if(start + 1 == end) {    // If there are only two arrays, merge them.
         return merge(kArrays[start], kArrays[end]);
     }
-    
     int mid = start + (end - start)/2;
-    // Divide the arrays into two halves.
     vector<int> first = mergeKSortedArraysHelper(kArrays, start, mid);
     vector<int> second = mergeKSortedArraysHelper(kArrays, mid + 1, end);
     
-    // Return the final merged array.
     return merge(first, second);
 }
 
 vector<int> mergeKSortedArrays(vector<vector<int>>&kArrays, int k) {
     vector<int> result = mergeKSortedArraysHelper(kArrays, 0, k - 1);
     return result;
+}
+
+
+
+
+
+
+
+
+
+
+
+// Approach - 3 (Optimal) using Heaps
+// T.C = O(n*k log(k))
+// S.C = O(k)[heap] + O(n*k)[result vector] = O(n*k)
+#include<queue>
+vector<int> mergeKSortedArrays(vector<vector<int>>&kArrays, int k) {
+    vector<int> result; 
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int> >>> minHeap; 
+    // O(klogk)
+    for (int i = 0; i < kArrays.size(); i++) {
+        minHeap.push({ kArrays[i][0], { i, 0 } }); 
+    }
+    // O(n*k log k)  -> n*k elements and insertion into heap takes log k
+    while (!minHeap.empty()) { 
+        pair<int, pair<int, int>> curr = minHeap.top();  // Remove the minimum element from the heap.
+        minHeap.pop(); 
+  
+        // i is the array number and j is the index of the removed element in the ith array.
+        int i = curr.second.first; 
+        int j = curr.second.second; 
+    
+        result.push_back(curr.first);     // Add the removed element to the output array.
+  
+        // If the next element of the extracted element exists, add it to the heap.
+        if (j + 1 < kArrays[i].size()) {
+            minHeap.push({ kArrays[i][j + 1], { i, j + 1 } }); 
+        }
+    }
+    return result; 
 }
