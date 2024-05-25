98bba6a60521632de220aec3c77a323d0e4bc50c
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-05-25 13:41:00 +0530

    Update 76_0.cpp

--- Lec_076/76_0.cpp
+++ Lec_076/76_0.cpp
@@ -2,7 +2,7 @@
 // K-th Largest Sum Subarray
 
 // Approach - 1 (BRUTE FORCE)
-// T.C = O(n^2(log n))
+// T.C = O(n^2(log(n^2)))
 #include<algorithm>
 int getKthLargest(vector<int> &arr, int k) {
     vector<int> sumStore;
@@ -29,18 +29,13 @@ int getKthLargest(vector<int> &arr, int k) {
 int getKthLargest(vector<int> &arr, int k) {
 	priority_queue<int, vector<int>, greater<int>> mini;
 	int n=arr.size();
-
 	for(int i=0; i<n ; i++) {
 		int sum=0;
 		for(int j=i; j<n; j++) {
 			sum+=arr[j];
-			if(mini.size()<k) {
-				mini.push(sum);
-			}
-			else {
-                if(sum>mini.top()) {
-				    mini.pop();
-				    mini.push(sum);
+			mini.push(sum);
+			if(mini.size()>k) {
+				mini.pop();
 			}
 		}
 	}
