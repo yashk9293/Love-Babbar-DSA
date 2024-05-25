8fd8d512db2319d3b6e23bd654840b79fb3fc429
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-05-25 15:31:44 +0530

    Update 33_3.cpp

--- Lec_033/33_3.cpp
+++ Lec_033/33_3.cpp
@@ -13,19 +13,18 @@ bool binarySearch(int *arr, int s, int e , int k ) {
     //base case
 
     //element not found
-    if(s>e)
+    if(s>e) {
         return false;
-
+    }
     int mid = s + (e-s)/2;
     cout << "Value of mid is : " << arr[mid] << endl;
     //element found
-    if(arr[mid] == k)
+    if(arr[mid] == k) {
         return true;
-
+    }
     if(arr[mid] < k) {
         return binarySearch(arr, mid+1, e, k);
-    }
-    else{
+    } else {
         return binarySearch(arr, s, mid-1, k);
     }
 }
