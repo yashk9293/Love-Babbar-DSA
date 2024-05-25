44a30c1081e611b66d3c03c884d1727c6f30b68d
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-05-25 13:33:09 +0530

    Update 74_2.cpp

--- Lec_074/74_2.cpp
+++ Lec_074/74_2.cpp
@@ -7,7 +7,7 @@ void heapify(int arr[], int n, int i) {
     int largest = i;
     int left = 2*i;
     int right = 2*i + 1;
-    if(left <= n && arr[largest] < arr[left]) {
+    if(left <= n && arr[largest] < arr[left]) {    // here left <= n, because we are considering 1-based indexing
         largest = left;
     }    
     if(right <= n && arr[largest] < arr[right]) {
