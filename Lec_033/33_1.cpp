27570ac055d530721cfe70a7682e343bf80b7a35
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-05-25 15:16:58 +0530

    Update 33_1.cpp

--- Lec_033/33_1.cpp
+++ Lec_033/33_1.cpp
@@ -1,16 +1,12 @@
 #include<iostream>
 using namespace std;
 
-int getSum(int *arr, int size) {
+int getSum(int *arr, int n) {
     //base case
-    if(size == 0) {
+    if(n <= 0) {
         return 0;
     }
-    if(size == 1 ) {
-        return arr[0];
-    }
-    int remainingPart = getSum(arr+1, size-1);
-    int sum = arr[0] + remainingPart;
+    int sum = arr[n-1] + getSum(arr, n-1);
 
     return sum;
 }
