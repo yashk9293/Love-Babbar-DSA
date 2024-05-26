238ed37a72922fd782d9e2799aa57fd270482ac8
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-05-26 20:23:14 +0530

    Update 39_1.cpp

--- Lec_039/39_1.cpp
+++ Lec_039/39_1.cpp
@@ -4,27 +4,23 @@
 // T.C = O(N! x N)
 // S.C = O(1)
 #include <bits/stdc++.h>
-void solve(string str,int index,vector<string>& ans){  // string is passed by value.
+void solve(int index, string &str, vector<string>& ans) {
     //base case
-    if(index >= str.length()){
+    if(index == str.length()){
         ans.push_back(str);
         return ;
     }
     for (int i = index; i < str.length(); i++) {
         swap(str[index], str[i]);
-        solve(str, index + 1, ans);
+        solve(index + 1, str, ans);
         // backtracking
-        swap(str[index], str[i]);   // the code will work even we do not use it as because each time 
-//                                     vector nums is passed by value  so  in every new call a new vector 
-//                                     will be created  so even  when the nums vector is changed in future 
-//                                     recursive calls it will not affect the previous nums vector.
+        swap(str[index], str[i]);
     }
 }
 
 vector<string> generatePermutations(string &str) {
     vector<string> ans;
-    int index=0;
-    solve(str,index,ans);
+    solve(0, str, ans);
     sort(ans.begin(), ans.end());
     return ans;
 }
