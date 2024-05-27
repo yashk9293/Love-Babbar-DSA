e734b4a6dfb70d7c9773969fc19185b27a645fe9
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-05-27 13:20:49 +0530

    Update 37_0.cpp

--- Lec_037/37_0.cpp
+++ Lec_037/37_0.cpp
@@ -1,8 +1,33 @@
 // Question Link :- https://leetcode.com/problems/subsets/
 // Subsets (Power Set, i.e., subsequence)
 
+// Approach - 1 (Bit manipulation)
+// T.C = O(2^n * n)
+// S.C = O(1)
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        for(int i=0; i<(1<<n); i++) {   // 1<<n = 2^n
+            vector<int> output;
+            for(int bit=0; bit<n; bit++){
+                if(i & (1<<bit)){
+                    output.push_back(nums[bit]);
+                }
+            }
+            ans.push_back(output);
+        }
+        return ans;
+    }
+};
+
+
+
+
+// Approach - 2 (Using Recursion)
 // T.C = O(2^n)
-// S.C = O(n) to store each subset
+// S.C = O(n)
 // The recursion stack space complexity is O(N) , the maximum depth of the recursion is N
 class Solution {
     private:
