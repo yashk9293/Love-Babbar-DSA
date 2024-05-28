2d67eefe3972c356976393764f24b038e34b466c
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-05-28 13:50:32 +0530

    Update 13_1.cpp

--- Lec_013/13_1.cpp
+++ Lec_013/13_1.cpp
@@ -1,91 +1,17 @@
 // Question Link :- https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
-// Peak Index
+// Peak Index in a Mountain Array
 
-#include<vector>
-using namespace std;
-
-class Solution {
-public:
-    int peakindex(vector<int> v) {
-        int s = 0, e = v.size() - 1;
-        int mid = (s + e) / 2; // or mid = s+(e-s)/2
-        while (s < e) { // s<=e no use as s=e is checking twice
-            if (v[mid] < v[mid + 1])
-                s = mid + 1;
-            else
-                e = mid; // here not e= mid - 1; because (mid-1)th element can be peak element
-
-            mid = (s + e) / 2; // or mid = s+(e-s)/2
-        }
-        return s;
-    }
-
-    int peakIndexInMountainArray(vector<int> &arr) {
-        return peakindex(arr);
-    }
-};
-
-// or 
-
+// Approach - 1 (Linear Search)
+// T.C = O(n)
+//S.C = O(1)
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int s=0;
-        int e=arr.size()-1;
-        int mid=s+(e-s)/2;
-
-        while(s<e){
-            if(arr[mid]<arr[mid+1]){
-                s=mid+1;
-            }
-            else{
-                e=mid;
-            }
-            mid=s+(e-s)/2;
-        }
-        return mid;
-    }
-};
-
-
-
-
-// ---------------- Method - 2 -------------------
-class Solution {
-public:
-    int peakIndexInMountainArray(vector<int>& a) {
-        int n = a.size();
-        //Peak exists somewhere in between start and end
-        int start = 1, end = n-2;     // Note -> end is going till n-2
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(a[mid] > a[mid-1] && a[mid] > a[mid+1]) 
-                return mid;
-            else if(a[mid-1] > a[mid]) 
-                end = mid-1;
-            else 
-                start = mid+1;
-        }
-        return -1;
-    }
-};
-
-
-
-
-// --------------- Method - 3 --------------------
-class Solution {
-public:
-    int peakIndexInMountainArray(vector<int>& arr) {
-        //using my own algorithm
-        int s=0, e=arr.size()-1;
-        int i=0;
-        while(s < e) {
-            if(arr[i] < arr[i+1]) {
-                i++;
-            }
-            else
-                break;
+        int n = arr.size();
+        int i = 0;
+       //You can remove i < n-1 check because array is gauranteed to be Mountain (given in qn)
+        while(i < n-1 && arr[i] < arr[i+1]) {
+            i++;
         }
         return i;
     }
@@ -93,7 +19,28 @@ public:
 
 
 
+// Approach - 2 (Binary Search)
+// T.C = O(logn)
+// S.C = O(1)
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int s=0;
+        int e=arr.size()-1;
+        int mid=s+(e-s)/2;
 
+        while(s<e) {     // s<=e no use as s=e is checking twice
+            if(arr[mid]<arr[mid+1]){
+                s=mid+1;
+            }
+            else{
+                e=mid;  // here not e= mid - 1; because (mid-1)th element can be peak element
+            }
+            mid=s+(e-s)/2;
+        }
+        return mid;
+    }
+};
 
 
 
