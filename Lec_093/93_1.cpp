d6f386d96bb0d0ac1eae5adb1f40c753f42692ed
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-08-01 09:11:45 +0530

    Update 93_1.cpp

--- Lec_093/93_1.cpp
+++ Lec_093/93_1.cpp
@@ -4,9 +4,41 @@
 // Count the shortest path in undirected unweighted graph. In question wrong image has been shown.
 // It is unweighted.
 
+
+
+// Approach - 1 (Floyd Warshell Algorithm)
+// Time Complexity: O(V^3)
+// Space Complexity: O(V^2)
+int shortestPath(int v, int e, vector<vector<int>> &edges, int src, int dest) {
+    vector<vector<int>> adj(v+1, vector<int>(v + 1, 1000000000));
+    for(int i=0; i<e; i++) {
+        int u = edges[i][0];
+        int v = edges[i][1];
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+
+    // Setting dist[index][index] as 0 as the distance between same vertices is 0.
+    for(int i=1; i<v+1; i++) {
+        adj[i][i] = 0;
+    }
+
+    for(int via=1; via <= v; via++) {
+        for(int i=1; i <= v; i++) {   // The variables i and j for traversing all possible pairs
+            for(int j = 1; j <= v; j++) {
+                adj[i][j] = min(adj[i][j], adj[i][via] + adj[via][j]);
+            }
+        }
+    }
+    return adj[src][dest];
+}
+
+
+
+
+// Approach - 2 (Using BFS)
 // T.C = O(V + E)
 // S.C = O(V + E)
-
 #include <bits/stdc++.h> 
 int shortestPath(int v, int e, vector<vector<int>> &edges, int source, int dest) {
     unordered_map< int, list<int> > adj;
