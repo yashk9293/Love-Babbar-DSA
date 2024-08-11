5fdb3d67d26c2db77354a640019927ef56727733
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-08-12 00:37:14 +0530

    Update 63_1.cpp

--- Lec_063/63_1.cpp
+++ Lec_063/63_1.cpp
@@ -1,71 +1,38 @@
-// Question Link :- https://practice.geeksforgeeks.org/problems/diameter-of-binary-tree/1
-// Diameter of a Binary Tree
+// Question Link :- https://leetcode.com/problems/diameter-of-binary-tree/
+// Diameter of Binary Tree (Aditya Verma)
 
+// T.C = O(n)
+// S.C = O(1)
+// as no additional space or data structures is created that is proportional to the input size of the tree. O(H) Recursive 
+// Stack Auxiliary Space: The recursion stack space is determined by the maximum depth of the recursion, which is the height of 
+// the binary tree denoted as H. In the balanced case it is log2N and in the worst case its N.
 
-// Approach - 1
-// T.C = O(N^2)
-// S.C = O(height)
 class Solution {
-  private:
-    int height(struct Node* node){
-        //base case
-        if(node == NULL) {
+public:
+    int solve(TreeNode* root, int &res) {
+        if(root == nullptr) {
             return 0;
         }
-        int left = height(node ->left);
-        int right = height(node->right);
-        
-        int ans = max(left, right) + 1;
+        int l = solve(root->left, res);
+        int r = solve(root->right, res);
 
-        return ans;
+        int temp = 1 + max(l,r);
+        int ans = max(temp, 1+l+r);
+        res = max(res, ans);
+
+        return temp;
     }
-  public:
-    // Function to return the diameter of a Binary Tree.
-    int diameter(Node* root) {
-        if(root == NULL) {
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(!root) {
             return 0;
         }
+        int res = INT_MIN;
+        solve(root, res);
+
+        return res-1;      // no. of edges
         
-        int op1= diameter(root->left);
-        int op2= diameter(root->right);
-        int op3 = height(root-> left) + 1 + height(root-> right);
-        
-        int ans = max(op1, max(op2, op3));
-        
-        return ans;
-    }
-};
-
-
-
-
-
-// Approach - 2
-// T.C = O(N)
-// S.C = O(height)
-class Solution {
-  public:
-    pair<int, int> diameterFast(Node* root) {
-        if(root == NULL) {
-            pair<int,int> p = make_pair(0,0);
-            return p;
-        }
-
-        pair<int, int> left = diameterFast(root->left);
-        pair<int, int> right = diameterFast(root->right);
-
-        int op1 = left.first;
-        int op2 = right.first;
-        int op3 = left.second + 1 + right.second;
-
-        pair<int, int> ans;
-        ans.first = max(op1, max(op2, op3));
-        ans.second = max(left.second, right.second) + 1;
-
-        return ans;
-    }
-
-    int diameter(Node* root) {
-        return diameterFast(root).first;
+        // Note:- aditya verma had found no.of nodes, but in leetcode we have to find no. of edges 
+        // so we have returned (res-1) in the function, for # nodes we would return res.
     }
 };
