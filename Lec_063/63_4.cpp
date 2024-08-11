b6764ebd08e9d19891ac0234f9fd8834152fe282
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-08-12 00:59:18 +0530

    Update 63_4.cpp

--- Lec_063/63_4.cpp
+++ Lec_063/63_4.cpp
@@ -4,41 +4,27 @@
 // T.C = O(N)
 // S.C = O(height of tree)
 class Solution {
-    pair<bool, int> isSumTreeFast(Node* root) {
-        if(root == NULL) {
-            pair<bool, int> p = make_pair(true, 0);
-            return p;
+  private:
+    
+  public:
+    int solve(Node* root, bool& res){
+        if(!root) {
+            return 0;
         }
-        
-        if(root-> left == NULL && root-> right == NULL) {
-            pair<bool, int> p = make_pair(true, root->data);
-            return p;
+        int leftSum = solve(root->left, res);
+        int rightSum = solve(root->right, res);
+        if(!root->left && !root->right) {
+            return root->data;
         }
-        
-        pair<bool, int> leftAns = isSumTreeFast(root->left);
-        pair<bool, int> rightAns = isSumTreeFast(root->right);
-        
-        bool isLeftSumTree  = leftAns.first;
-        bool isRightSumTree  = rightAns.first;
-        
-        int leftSum = leftAns.second;
-        int rightSum = rightAns.second;
-        
-        bool condn = root->data == leftSum + rightSum;
-        pair<bool, int> ans;
-        
-        if(isLeftSumTree  && isRightSumTree  && condn) {
-            ans.first = true;
-            ans.second = root->data + leftSum + rightSum;
+        if(root->data != leftSum + rightSum) {
+            return res = false;
         }
-        else {
-            ans.first = false;
-        }
-        
-        return ans;
+        return root->data + leftSum + rightSum;
     }
-    public:
+    
     bool isSumTree(Node* root) {
-         return isSumTreeFast(root).first;
+        bool res = true;
+        solve(root, res);
+        return res;
     }
 };
