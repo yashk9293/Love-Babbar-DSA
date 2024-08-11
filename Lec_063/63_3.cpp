20aa8b58e8e196d2313c97006eb90b9576342d14
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-08-12 01:09:29 +0530

    Update 63_3.cpp

--- Lec_063/63_3.cpp
+++ Lec_063/63_3.cpp
@@ -1,8 +1,8 @@
 // Question Link :- https://practice.geeksforgeeks.org/problems/determine-if-two-trees-are-identical/1
 // Determine if Two Trees are Identical
 
-// T.C = O(N)
-// S.C = O(N)
+// T.C = O(n+m)
+// S.C = O(height)
 class Solution {
     public:
     bool isIdentical(Node *r1, Node *r2) {
@@ -26,5 +26,36 @@ class Solution {
         else {
             return false;
         }
+    }
+};
+
+
+
+
+
+
+
+
+
+// Question Link :- https://leetcode.com/problems/same-tree/
+// Same Tree
+
+// T.C = O(n+m)
+// S.C = O(height)
+class Solution {
+public:
+    bool check(TreeNode* node1, TreeNode* node2) {
+        if(node1 == NULL && node2 == NULL) {
+            return true;
+        }
+        // If only one of the nodes is NULL, they are not identical
+        if(node1 == NULL || node2 == NULL) {
+            return false;
+        }
+        return ((node1->val == node2->val) && check(node1->left, node2->left) && check(node1->right, node2->right));
+    }
+
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return check(p, q);
     }
 };
