2a5bcb194f2debf9243bc51462bd94d4b87e8888
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-08-13 02:20:19 +0530

    Update 64_5.cpp

--- Lec_064/64_5.cpp
+++ Lec_064/64_5.cpp
@@ -1,13 +1,18 @@
 // Question Link :- https://practice.geeksforgeeks.org/problems/left-view-of-binary-tree/1
 // Left View of Binary Tree
 
+// T.C = O(N)
+// For a balanced binary tree, the height is log2N but in the worst case when the tree is skewed, the complexity becomes O(N).
+
+// S.C = O(N)
+// The size of this result vector is proportional to the height of the Binary Tree which will be log2N when the tree is balanced and O(N) in the worst case of a skewed tree.
+
+
+// Approach - 1 (Pre Order Traversal)
 void solve(Node *root, vector<int> &ans, int level) {
-    // base case
     if (root == NULL) {
         return;
     }
-
-    // we entered into a new level
     if (level == ans.size()) {
         ans.push_back(root->data);
     }
@@ -18,5 +23,34 @@ void solve(Node *root, vector<int> &ans, int level) {
 vector<int> leftView(Node *root) {
     vector<int> ans;
     solve(root, ans, 0);
+    return ans;
+}
+
+
+
+
+
+// Approach - 2 (Level Order Traversal)
+vector<int> leftView(Node *root) {
+   vector<int> ans;   // modification 1
+    if(root == NULL) {
+        return ans;
+    }
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()) {
+        int size = q.size();
+        ans.push_back(q.front()->data);  // modification 2
+        for(int i=0; i<size; i++) {
+            Node *node = q.front();
+            q.pop();
+            if(node->left) {
+                q.push(node->left);
+            }
+            if(node->right) {
+                q.push(node->right);
+            }
+        }
+    }
     return ans;
 }
