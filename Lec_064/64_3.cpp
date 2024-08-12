2473550042b0356e073d1a147a1f643bab2db88c
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-08-13 02:10:35 +0530

    Update 64_3.cpp

--- Lec_064/64_3.cpp
+++ Lec_064/64_3.cpp
@@ -1,45 +1,35 @@
 // Question Link :- https://practice.geeksforgeeks.org/problems/top-view-of-binary-tree/1
 // Top View of Binary Tree
 
+// T.C = O(N)
+// S.C = O(N)
 class Solution {
     public:
-    //Function to return a list of nodes visible from the top view 
-    //from left to right in Binary Tree.
     vector<int> topView(Node *root) {
         vector<int> ans;
-        
         if(root == NULL) {
             return ans;
         }
-        
-        map<int, int> topNode;
-        queue<pair<Node*, int>> q;
-        
-        q.push(make_pair(root, 0));
-        
+        map<int, int> mpp;  // {line, node->val}
+        queue<pair<Node*, int>> q;  // {node, line}
+        q.push({root, 0});
         while(!q.empty()) {
-            pair<Node*, int> temp = q.front();
+            auto it = q.front();
             q.pop();
-            Node* frontNode = temp.first;
-            int hd = temp.second;
-            
-            
-            // if one value is present for a HD, then do nothing
-            if(topNode.find(hd) == topNode.end()) {
-                topNode[hd] = frontNode -> data;
+            Node* node = it.first;
+            int line = it.second;
+            if(mpp.find(line) == mpp.end()) {
+                mpp[line] = node->data;
             }
-            
-            if(frontNode->left) {
-                q.push(make_pair(frontNode->left, hd-1));
+            if(node->left != NULL) {
+                q.push({node->left, line - 1});
             }
-            
-            if(frontNode->right) {
-                q.push(make_pair(frontNode->right, hd+1));
+            if(node->right != NULL) {
+                q.push({node->right, line + 1});
             }
         }
-        
-        for(auto i : topNode) {
-            ans.push_back(i.second);
+        for(auto it : mpp) {
+            ans.push_back(it.second);
         }
         return ans;
     }
