1318288c1c48326ac7a46d556ab8f98a35cb91a0
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-08-13 00:47:43 +0530

    Update 64_2.cpp

--- Lec_064/64_2.cpp
+++ Lec_064/64_2.cpp
@@ -1,59 +1,54 @@
 // Question Link :- https://practice.geeksforgeeks.org/problems/print-a-binary-tree-in-vertical-order/1
-// Vertical Traversal of Binary Tree
+// Vertical Traversal of Binary Tree (Striver)
 
-// doc link - https://drive.google.com/file/d/1MHf_W1V7gW3t_ge8hDxRTyQb8mAjI7D8/view
+// NOTE : If there are multiple nodes passing through a vertical line, then they should be printed 
+// as they appear in level order traversal of the tree, that's why we use 'vector' in the map..
 
-/*
-                                     -2 -1   0   1  2  3
 
-                                             1
-                                            / \
-                                           /   \
-                                          /     \
-                                         2       3
-                                        / \     / \
-                                       /   \   /   \
-                                      4     5 6     7
-                                               \     \
-                                                \     \
-                                                 8     9
-*/
+
+// T.C = O(NlogN) where N represents the number of nodes in the Binary Tree.
+// For BFS Traversal -> O(N) and for insertion into map O(logN). So O(NlogN)
+
+// The outer loop runs for each unique vertical index (let's say there are V such verticals) -> O(V)
+
+// The inner loop runs for each unique level index within a vertical (lets say there are
+// L levels per vertical on average) -> O(L)
+
+// For each level, the multiset of node values is traversed. Since the multiset might store up to 
+// N nodes, inserting them into the vector col takes  O(N) operations in the worst case.
+
+// So O(V*L*N) ~ O(N) for traversing
+
+// S.C = O(N + N/2) -> map + queue(the maximum level of the tree which can be O(N/2) in the worst case of a balanced tree)
 
 
 class Solution {
     public:
-    //Function to find the vertical order traversal of Binary Tree.
     vector<int> verticalOrder(Node *root) {
-        map<int, map<int, vector<int>>> nodes;
-        queue<pair<Node*, pair<int,int>>> q;
-        vector<int>ans;
-        
-        if(root== NULL) {
-            return ans;
-        }
-        
-        q.push(make_pair(root, make_pair(0,0)));
-        
-        while(!q.empty()) {
-            pair<Node*, pair<int, int>> temp = q.front();
+        map<int, map<int, vector<int>>> mp;  // {vertical, level, multinodes}
+        queue<pair<Node*, pair<int, int>>> q;  // {node, vertical, level}
+        q.push({root, {0, 0}});  
+        // BFS traversal
+        while(!q.empty()){
+            auto p = q.front();
             q.pop();
-            Node* frontNode = temp.first;
-            int hd = temp.second.first;
-            int lvl = temp.second.second;
-            
-            nodes[hd][lvl].push_back(frontNode->data);
-            
-            if(frontNode->left)
-                q.push(make_pair(frontNode->left, make_pair(hd-1, lvl+1)));
-                
-            if(frontNode->right)
-                q.push(make_pair(frontNode->right, make_pair(hd+1, lvl+1)));
+            Node* temp = p.first;
+            int x = p.second.first;  // vertical
+            int y = p.second.second; // level
+            mp[x][y].push_back(temp->data);  // mp[vertical][level] = nodes
+            if(temp->left) {
+                q.push({temp->left, {x-1, y+1}});
+            }
+            if(temp->right) {
+                q.push({temp->right, {x+1, y+1}});
+            }
         }
-        
-        for(auto i: nodes) {
-            for(auto j:i.second) {
-                for(auto k:j.second) {
-                    ans.push_back(k);
+        vector<int> ans;
+        for(auto x: mp) {  // x = int, map<int, multiset<int>>
+            for(auto y: x.second) {  // x.second = map<int, multiset<int>> and y = int, multiset<int>
+                // ans.insert(ans.end(), y.second.begin(), y.second.end());
+                for(auto z : y.second) {  // y.second = multiset<int> and z = int
+                    ans.push_back(z);
                 }
             }
         }
