e8954c0229e944bc520145a480be0792b9546d04
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-08-14 15:07:01 +0530

    Update 65_2.cpp

--- Lec_065/65_2.cpp
+++ Lec_065/65_2.cpp
@@ -39,29 +39,34 @@ class Solution{
 
 // T.C = O(n) 
 // S.C = O(height)
-class Solution{
+class Solution {
   public:
-    void solve(Node *root,int k,map<int,int>& mp,int &count,int &sum){
+    void solve(Node *root, int k, int currentSum, unordered_map<int, int>& mp, int &count) {
         if (root == NULL) {
-            return ;
+            return;
         }
-        sum += root->data;
-        if (mp.find(sum-k) != mp.end()) {
-            count += mp[sum-k];
+        //subarray sum equals k jaisa code hai
+        currentSum += root->data;
+        if (currentSum == k) {
+            count++;
         }
-        mp[sum]++;
-        solve(root->left, k, mp, count, sum);
-        solve(root->right, k, mp, count, sum);
-        mp[sum]--;
-        sum -= root->data;
+        if (mp.find(currentSum - k) != mp.end()) {
+            count += mp[currentSum - k];
+        }
+        // increment karega agar map me nhi mila to (+1) se
+        mp[currentSum]++;
+
+        solve(root->left, k, currentSum, mp, count);
+        solve(root->right, k, currentSum, mp, count);
+
+       // backtrack karega..agar last pe pahuch gaya hai to piche jayega to sum me se bhi minus hoga..aur freq bhi
+        mp[currentSum]--;
     }
 
-    int sumK(Node *root,int k) { 
+    int sumK(Node *root, int k) {
+        unordered_map<int, int> mp;
         int count = 0;
-        map<int,int> mp;
-        mp[0] = 1;
-        int sum = 0;
-        solve(root, k, mp, count, sum);
+        solve(root, k, 0, mp, count);
         return count;
     }
 };
