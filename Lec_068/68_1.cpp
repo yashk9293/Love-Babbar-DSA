d7bce09823b2deffaad8a5940494eaf89426ec9b
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-08-21 20:25:38 +0530

    Update 68_1.cpp

--- Lec_068/68_1.cpp
+++ Lec_068/68_1.cpp
@@ -1,64 +1,32 @@
+// Question Link :- https://leetcode.com/problems/binary-tree-preorder-traversal
+// Binary Tree Preorder Morris Traversal
+
 // T.C = O(N)
 // S.C = O(1)
-
-
-// Morris Traversal for inorder
-vector<int> getInorder(TreeNode* root) {
-    vector<int> inorder;
-    TreeNode *curr=root;
-    while(curr != NULL) {
-        if(curr -> left == NULL) {
-            inorder.push_back(curr->val);
-            curr=curr->right;
-        }
-        else {
-            TreeNode *prev = curr->left;
-            while(prev->right && prev->right != curr) {
-                prev = prev->right;
-            }
-            if(prev->right == NULL) {
-                prev->right = curr;
-                curr=curr->left;
-            }
-            else{
-                prev->right=NULL;
-                inorder.push_back(curr->val);
-                curr=curr->right;
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        TreeNode* curr = root;
+        while (curr != NULL) {
+            if (curr->left == NULL) {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                TreeNode* temp = curr->left;
+                while (temp->right != NULL && temp->right != curr) {
+                    temp = temp->right;
+                }
+                if (temp->right == NULL) {
+                    ans.push_back(curr->val);
+                    temp->right = curr;
+                    curr = curr->left;
+                } else {
+                    temp->right = NULL;
+                    curr = curr->right;
+                }
             }
         }
-        return inorder;
+        return ans;
     }
-}
-
-
-
-
-
-
-// Morris Traversal for preorder
-vector<int> getInorder(TreeNode* root) {
-    vector<int> preorder;
-    TreeNode *curr=root;
-    while(curr != NULL) {
-        if(curr -> left == NULL) {
-            inorder.push_back(curr->val);
-            curr=curr->right;
-        }
-        else {
-            TreeNode *prev = curr->left;
-            while(prev->right && prev->right != curr) {
-                prev = prev->right;
-            }
-            if(prev->right == NULL) {
-                prev->right = curr;
-                preorder.push_back(curr->val);   // 1 line change from inorder
-                curr=curr->left;
-            }
-            else{
-                prev->right=NULL;
-                curr=curr->right;
-            }
-        }
-        return preorder;
-    }
-}
+};
