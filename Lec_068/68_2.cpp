5ee2e9ceced9ac7bf8bb9158362ce39dc08f3a05
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-08-23 11:23:48 +0530

    Update 68_2.cpp

--- Lec_068/68_2.cpp
+++ Lec_068/68_2.cpp
@@ -1,69 +1,49 @@
-// Question Link :- https://practice.geeksforgeeks.org/problems/flatten-binary-tree-to-linked-list/1
-// Flatten binary tree to linked list
-
-   /*
-                   1
-                  / \
-                 2   5
-                / \   \
-               3   4   6
-   */
+// Question Link :- https://leetcode.com/problems/flatten-binary-tree-to-linked-list
+// Flatten Binary Tree to Linked List
 
 
-// Approach - 1 (using Stack - striver)
-// S.C = O(N)
+// Approach - 1 (Recursive)
 // T.C = O(N)
-st.push(root);
-while(!st.empty()) {
-    curr=s.top();
-    st.pop();
-    if(curr->right) {
-        st.push(curr->right);
-    }
-    if(curr->left) {
-        st.push(curr->left);
-    }
-    if(!st.empty()) {
-        curr->right=st.top();
-    }
-    curr->left=NULL;
-}
-
-
-// Approach - 2 (using recursion - striver)
 // S.C = O(N)
-// T.C = O(N)
-prev=NULL;
-flatten(node){
-    if(node==x) {
-        return;
-    }
-    flatten(node->right);
-    flatten(node->left);
-    node->right=prev;
-    node->left=null;
-    prev=node;
-}
-
-
-
-// Approach-3 (Morris Traversal)
 class Solution {
-    public:
-    void flatten(Node *root) {
-        Node* curr = root;
-        while(curr!= NULL) {
-            if(curr->left) {
-                Node* pred=curr->left;
-                while(pred->right) {
-                    pred=pred->right;
-                }
-                pred->right=curr->right;
-                curr->right=curr->left;
-                curr->left=NULL;
-            }
-            curr=curr->right;
+public:
+    TreeNode* prev = NULL;
+    void flatten(TreeNode* root) {
+        if(root == NULL) {
+            return;
         }
+        flatten(root->right);
+        flatten(root->left);
+
+        root->right = prev;
+        root->left = NULL;
+        prev = root;
     }
 };
 
+
+
+
+
+// Approach - 2 (Morris Traversal)
+// T.C = O(N)
+// S.C = O(1)
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        while(root != NULL){
+            if(root->left == NULL){
+               root = root->right;
+            } else {
+                TreeNode* temp = root->left;
+                while(temp->right){
+                  temp = temp->right;
+                }
+                temp->right = root->right;
+                root->right = root->left;
+                root->left = NULL;
+                root = root->right;
+            }
+        }
+    }
+};
