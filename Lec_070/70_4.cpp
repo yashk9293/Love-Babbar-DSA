d248e1600078c4115e3eb817805db90a57cbe12d
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-08-24 23:27:26 +0530

    Update 70_4.cpp

--- Lec_070/70_4.cpp
+++ Lec_070/70_4.cpp
@@ -1,37 +1,49 @@
-// Question Link :- https://www.codingninjas.com/studio/problems/lca-in-a-bst_981280
-// LCA of Two Nodes In A BST
+// Question Link :- https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree
+// Lowest Common Ancestor of a Binary Search Tree
 
-// Approach - 1 (Recursive)
-// T.C = O(n)
+
+// Approach - 1 (Iterative)
+// T.C = O(H)
 // S.C = O(1)
-Node* LCAinBST(Node* root, Node* P, Node* Q) {
-    if(root == NULL) {
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while (root != NULL) {
+            if (root->val > p->val && root->val > q->val) {  // p, q belong to the left subtree
+                root = root->left;
+            }
+            else if (root->val < p->val && root->val < q->val) {  // p, q belong to the right subtree
+                root = root->right;
+            }
+            else {  // Now, small <= root.val <= large -> This root is the LCA between p and q
+                return root;
+            }
+        }
         return NULL;
     }
-    if(root->data < P->data && root->data < Q->data) {
-        LCAinBST(root->right, P, Q);
-    }
-    if(root->data > P->data && root->data > Q->data) {
-        LCAinBST(root->left, P, Q);
-    }
-
-    return root;
-}
+};
 
 
-// Approach - 2 (Iterative)
-// T.C = O(n)
+
+
+
+// Approach - 2 (Recursive)
+// T.C = O(H)
 // S.C = O(1)
-Node* LCAinBST(Node* root, Node* P, Node* Q) {
-    while(root != NULL) {
-        if(root->data < P->data && root->data < Q->data) {
-            root = root->right;
-        } 
-        else if (root->data > P->data && root->data > Q->data) {
-            root = root->left;
-        } 
-        else {
-            return root;
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL) {
+            return NULL;
         }
+        int curr = root->val;
+        if(curr < p->val && curr < q->val) {
+            return lowestCommonAncestor(root->right, p, q);
+        }
+        if(curr > p->val && curr > q->val) {
+            return lowestCommonAncestor(root->left, p, q);
+        }
+        // if not the above case then we got the lca (either of the p or q is the current node or only one of the node is on left or right side not both of them ) hence the current node will be lca
+        return root;
     }
-}
+};
