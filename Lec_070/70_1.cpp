342b7e6dae030abebf8eeaf09713e03968e33db8
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-08-24 23:27:00 +0530

    Update 70_1.cpp

--- Lec_070/70_1.cpp
+++ Lec_070/70_1.cpp
@@ -1,38 +1,8 @@
 // Question Link :- https://www.codingninjas.com/studio/problems/find-k-th-smallest-element-in-bst_1069333
 // Find K-th smallest Element in BST
 
-// Approach - 1
-// T.C = O(n)
-// S.C = O(h)
-int solve(BinaryTreeNode<int>* root, int k, int &i) {
-    if(root == NULL) {
-        return -1;
-    }
-    // L
-    int left = solve(root->left, k, i);
 
-    if(left != -1) {
-        return left;
-    }
-    // N
-    i++;
-    if(i==k) {
-        return root->data;
-    }
-
-    // R
-    return solve(root->right, k, i);
-}
-
-int kthSmallest(BinaryTreeNode<int>* root, int k) {
-    int i=0;
-    int ans = solve(root, k, i);
-    return ans;
-}
-
-
-
-// Approach -2
+// Approach - 1 (Inorder)
 // T.C = O(n)
 // S.C = O(n)
 void inorderTraversal(Node* root, vector<int> &ans) {
@@ -43,14 +13,40 @@ void inorderTraversal(Node* root, vector<int> &ans) {
     ans.push_back(root->data);
     inorderTraversal(root->right, ans);
 }
-  
-// Return the Kth smallest element in the given BST
+
 int KthSmallestElement(Node *root, int K) { 
     vector<int> ans;
-    inorderTraversal(root,ans);
+    inorderTraversal(root, ans);
     if(ans.size() < K) {
         return -1;
     }
-
     return ans[K-1];
+}
+
+
+
+
+// Approach - 2 (Recursion)
+// T.C = O(n)
+// S.C = O(1)
+void solve(BinaryTreeNode<int>* root, int& k, int& ans) {
+    if(root == NULL) {
+        return;
+    }
+    solve(root->left, k, ans);
+    k--;
+    if(k == 0) {
+        ans = root->data;
+        return;
+    }
+    solve(root->right, k, ans);
+}
+
+int kthSmallest(BinaryTreeNode<int>* root, int k) {
+    int ans = -1;
+    solve(root, k, ans);
+    if(ans == -1) {
+        return -1;
+    }
+    return ans;
 }
