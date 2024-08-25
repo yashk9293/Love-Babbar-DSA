f31727d2a5aeac1d9228eec4f78206c18048bb3d
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-08-25 11:57:17 +0530

    Update 69_4.cpp

--- Lec_069/69_4.cpp
+++ Lec_069/69_4.cpp
@@ -1,39 +1,39 @@
-// Binary Search Tree Inorder Successor and Predecessor
+// Question Link :- https://www.geeksforgeeks.org/problems/inorder-successor-in-bst/1
+// Inorder Successor in BST
+
 // S.C = O(h)
 // T.C = O(1)
 
 class Solution {
-public:
-    TreeNode * inorderPredecessor(TreeNode * root, TreeNode * p) {
-        TreeNode* successor = NULL;
-        while(root!=NULL){
-            if(p->val >= root->val){
+  public:
+    Node * inOrderSuccessor(Node *root, Node *x) {
+        Node* suc = NULL;
+        while(root != NULL) {
+            if(root->data <= x->data) {
                 root = root->right;
-            }
-            else{
-                successor = root;
+            } else {
+                suc = root;
                 root = root->left;
             }
         }
-        return successor;
+        return suc;
     }
 };
 
 
-
+// Inorder Predecessor in BST
 class Solution {
 public:
-    TreeNode * inorderPredecessor(TreeNode * root, TreeNode * p) {
-        TreeNode* predecessor = NULL;
-        while(root!=NULL){
-            if(root->val >= p->val){
+    TreeNode* inorderPredecessor(TreeNode* root, TreeNode* p) {
+        TreeNode* pre = NULL;
+        while(root != NULL) {
+            if(root->val >= p->val) {
                 root = root->left;
-            }
-            else{
-                predecessor = root;
+            } else {
+                pre = root;
                 root = root->right;
             }
         }
-        return predecessor;
+        return pre;
     }
 };
