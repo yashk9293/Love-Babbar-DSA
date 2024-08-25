af67b4f3fef986d1ed969705dab33629c7ee6fe2
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-08-25 20:20:37 +0530

    Update 70_0.cpp

--- Lec_070/70_0.cpp
+++ Lec_070/70_0.cpp
@@ -1,38 +1,46 @@
 // Question Link :- https://www.codingninjas.com/studio/problems/validate-bst_799483
 // Partial BST
 
-// -The left subtree of a node contains only nodes with data less than and equal to the node's data.
-// -The right subtree of a node contains only nodes with data less than and equal to the node's data.
-// -Both the left and right subtrees must also be the partial binary search trees.
-
 // Approach - 1
 // T.C = O(n)
 // S.C = O(n)
-// Travel the BST in the inorder traversal, and store the elements in an array or vector, and check whether
-// they are in ascending order. If yes than return true else return false.
+void solve(BinaryTreeNode<int>* root, vector<int>&ans) {
+    if(root == NULL) {
+        return;
+    }
+    solve(root->left, ans);
+    ans.push_back(root->data);
+    solve(root->right, ans);
+}
+
+bool validateBST(BinaryTreeNode<int> *root) {
+    vector<int>ans;
+    solve(root, ans);
+    for(int i=0; i<ans.size()-1; i++) {
+        if(ans[i] >= ans[i+1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
 
 // Approach - 2
 // T.C = O(n)
 // S.C = O(1)
-
-bool isBST(BinaryTreeNode<int> *root, int min, int max) {
-    // base case
+bool isBST(BinaryTreeNode<int> *root, long mini, long maxi) {
     if(root == NULL) {
         return true;
     }
-
-    if(root->data >= min && root->data <= max) {
-        bool left = isBST(root->left, min, root->data);
-        bool right = isBST(root->right, root->data, max);
-        return left && right;
-    }
-    else {
+    if(root->data < mini || root->data > maxi) {
         return false;
     }
+    bool left = isBST(root->left, mini, root->data);
+    bool right = isBST(root->right, root->data, maxi);
+    return left && right;
 }
 
-
 bool validateBST(BinaryTreeNode<int> *root) {
-    bool ans = isBST(root, INT_MIN, INT_MAX);
-    return ans;
+    return isBST(root, LLONG_MIN, LLONG_MAX);
 }
