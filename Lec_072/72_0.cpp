e513f77badd5576d91b07afa34d96c96b522baec
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-08-26 14:11:15 +0530

    Update 72_0.cpp

--- Lec_072/72_0.cpp
+++ Lec_072/72_0.cpp
@@ -5,6 +5,7 @@
 // step 1 : store the inorder of both the bst
 // step 2 : merge two sorted arrays
 // step 3 : make bst from inorder sorted array
+
 // T.C = O(m+n)
 // S.C = O(m+n)
 
@@ -12,7 +13,6 @@ void inOrder(Node* root, vector<int>& ans) {
     if(root == NULL) {
         return;
     }
-
     inOrder(root->left, ans);
     ans.push_back(root->data);
     inOrder(root->right, ans);
@@ -22,7 +22,6 @@ vector<int> mergeArrays(vector<int> arr1, vector<int> arr2) {
     if(arr1.size() == 0 || arr2.size() == 0) {
         return (arr1.size()) ? arr1 : arr2;
     }
-
     int i=0, j=0;
     vector<int> solution;
 
@@ -35,43 +34,37 @@ vector<int> mergeArrays(vector<int> arr1, vector<int> arr2) {
             j++;
         }
     }
-    
-    while(i<arr1.size()) {
+    while(i < arr1.size()) {
         solution.push_back(arr1[i]);
         i++;
     }
-    
-    while(j<arr2.size()) {
+    while(j < arr2.size()) {
         solution.push_back(arr2[j]);
         j++;
     }
-
     return solution;
 }
 
-Node* createBST(vector<int> &arr, int start, int end) {
-    if(start > end) {
+
+Node* makeBST(vector<int>& result, int l, int r) {
+    if(l > r) {
         return NULL;
     }
-
-    int mid = start + (end-start)/2;
-    Node* temp = new Node(arr[mid]);
-
-    temp->left = createBST(arr, start, mid-1);
-    temp->right = createBST(arr, mid+1, end);
-
-    return temp;
+    int mid = (l+r)/2;
+    Node* node = new Node(result[mid]);
+    node->left = makeBST(result, l, mid-1);
+    node->right = makeBST(result, mid+1, r);
+    return node;
 }
 
 Node* MergeBST(Node* root1, Node* root2) {
     vector<int> arr1;
     vector<int> arr2;
-
     inOrder(root1, arr1);
     inOrder(root2, arr2);
 
-    vector<int> resultArray = mergeArrays(arr1, arr2);
-    Node* answer = createBST(resultArray, 0, resultArray.size()-1);
+    vector<int> result = mergeArrays(arr1, arr2);
+    Node* answer = makeBST(result, 0, result.size()-1);
 
     return answer;
 }
