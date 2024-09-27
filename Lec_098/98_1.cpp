d9baa0b98f790fde94c7f81c3ef387e48b63b63b
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-09-27 20:38:39 +0530

    Update 98_1.cpp

--- Lec_098/98_1.cpp
+++ Lec_098/98_1.cpp
@@ -29,7 +29,7 @@ public:
                 }
             } else {
                 // Back edge found
-                low[node] = min(low[node], disc[neigh]);
+                low[node] = min(low[node], low[neigh]);
             }
         }
     }
