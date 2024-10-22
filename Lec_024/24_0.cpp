4046069a32d2df6c6dd754691eb0b541a3f35744
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-10-22 15:05:13 +0530

    Update 24_0.cpp

--- Lec_024/24_0.cpp
+++ Lec_024/24_0.cpp
@@ -1,36 +1,41 @@
-// Question Link :- https://leetcode.com/problems/count-primes/
-// tc - O(n log(logn))
+// Question Link :- https://leetcode.com/problems/count-primes/description/
+// Count Primes
 
-// BRUTE FORCE
-#include<iostream>
-using namespace std;
-
-bool isPrime(int n) {
-    if(n<=1)
-    return false;
-    for(int i=2; i<n; i++) {
-        if(n%i==0) {
+// Brute Force
+// T.C = O(N*sqrt(N))
+// S.C = O(1)
+class Solution {
+public:
+    bool checkPrime(int n) {     // O(sqrt(N))
+        int cnt = 0;
+        for (int i=2; i*i <= n; i++) {
+            if (n % i == 0) {
+                cnt++;
+            }
+        }
+        if (cnt > 0) {
             return false;
+        } else {
+           return true;
         }
     }
-    return true;
-}
-int main() {
-    int n;
-    cin >> n;
-    if(isPrime(n)) {
-        cout << "It is a Prime Number" << endl;
+
+    int countPrimes(int n) {
+        int cnt = 0;
+        for(int i=2; i<n; i++) {   // O(N)
+            if(checkPrime(i)) {
+                cnt++;
+            }
+        }
+        return cnt;
     }
-    else{
-        cout << " It is not a Prime Number" << endl;
-    }
-    return 0;
-}
+};
 
 
 
-
-// Sieve of Eratosthenes
+// Better Approach
+// T.C = O(NlogN)
+// S.C = O(N)
 class Solution {
 public:
     int countPrimes(int n) {
@@ -39,13 +44,12 @@ public:
 
         prime[0]=prime[1] = false;
 
-        for(int i=2; i<n; i++) {
+        for(int i=2; i<n; i++) {   // O(N)
             if(prime[i]) {
                 cnt++;
-            }
-
-            for(int j=2*i; j<n; j=j+i) {
-                prime[j] = false;
+                for(int j=2*i; j<n; j=j+i) {  // O(logN) harmonic series
+                    prime[j] = 0;
+                }
             }
         }
         return cnt;
@@ -55,85 +59,29 @@ public:
 
 
 
+// Optimised Better Approach
+// T.C = O(N*log(logN))
+// S.C = O(N)
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool> prime(n+1, true);
+        prime[0] = prime[1] = false;
 
-
-
-
-// using while loop
-int countPrimes(int n) {
-
-    if (n == 0)
-        return false;
-
-    vector<bool> prime(n, true);
-    prime[0] = prime[1] = false;
-    int ans = 0;
-
-    for (int i = 2; i < n; i++) {
-        if (prime[i]) {
-            ans++;
-            int j = 2 * i;
-            while (j < n) {
-                prime[j] = false;
-                j += i;
+        for(int i=2; i*i<n; i++) {     // modification 1: for(2 to sqrt(n))
+            if(prime[i]) {
+                for(int j= i*i; j<n; j+=i) {    // modification 2: start j from i*i
+                    prime[j] = 0;
+                }
             }
         }
+        int cnt = 0;
+        // Count primes by checking remaining 1s in the vector.
+        for (int i = 2; i < n; i++) {
+            if (prime[i] == 1) {
+                cnt++;
+            }
+        }
+        return cnt;
     }
-    return ans;
 };
-
-
-
-
-
-
-
-
-
-// same logic other way writing code
-#include<iostream>
-#include<vector>
-using namespace std;
-
-//using Sieve
-void sieve(bool prime[]) {
-
-    //setting TRUE for every number
-    for(int i=2;i<=1000000;i++) {
-        prime[i]=1;
-    }
-    for(int i = 2; i<=1000000; i++) {
-        if(prime[i]){
-            //iske saare multiple mark krde - non prime
-            for(int j = 2*i; j<=1000000; j+=i){
-                prime[j] = 0;   
-            }
-        }
-    }
-    prime[0] = prime[1] = 0;
-}
-
-int main() {
-    bool prime[1000001];
-    sieve(prime);
-
-    int n;
-    cin >> n;
-    while(n!=-1) {
-        if(prime[n]) {
-            cout << "It is a Prime Number" << endl;
-        }
-        else {
-            cout << " It is not a Prime Number" << endl;
-        }
-        cin >> n;
-    }
-    return 0;
-}
-
-
-
-
-// Input: n = 10
-// Output: 4
-// Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
