88a783941adcbd1b47cd15c471aa8f9dff09ed1b
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-11-03 12:52:28 +0530

    Update 35_0.cpp

--- Lec_035/35_0.cpp
+++ Lec_035/35_0.cpp
@@ -1,4 +1,6 @@
+// Question Link :- https://www.geeksforgeeks.org/problems/merge-sort/1
 // MERGE SORT - STRIVER 
+
 // Merge sort is a Recursive Algorithm that continuously splits the array in half until it cannot be further divided i.e., 
 // the array has only one element left (an array with one element is always sorted). Then the sorted subarrays are merged 
 // into one sorted array.
@@ -13,70 +15,47 @@
 // Auxiliary Space: O(N), In merge sort all elements are copied into an auxiliary array. So N auxiliary space is required for 
 // merge sort.
 
-#include <bits/stdc++.h>
-using namespace std;
-
-void merge(vector<int> &arr, int low, int mid, int high) {
-    vector<int> temp;
-    int left = low;      // starting index of left half of arr
-    int right = mid + 1;   // starting index of right half of arr
-
-    while (left <= mid && right <= high) {
-        if (arr[left] <= arr[right]) {
+class Solution {
+  public:
+    void merge(vector<int> &arr, int low, int mid, int high) {
+        vector<int> temp;
+        int left = low;      // starting index of left half of arr
+        int right = mid + 1;   // starting index of right half of arr
+    
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.push_back(arr[left]);
+                left++;
+            }
+            else {
+                temp.push_back(arr[right]);
+                right++;
+            }
+        }
+        // if elements on the left half are still left //
+        while (left <= mid) {
             temp.push_back(arr[left]);
             left++;
         }
-        else {
+        //  if elements on the right half are still left //
+        while (right <= high) {
             temp.push_back(arr[right]);
             right++;
         }
-    }
-    // if elements on the left half are still left //
-    while (left <= mid) {
-        temp.push_back(arr[left]);
-        left++;
-    }
-    //  if elements on the right half are still left //
-    while (right <= high) {
-        temp.push_back(arr[right]);
-        right++;
+    
+        // transfering all elements from temporary to arr //
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp[i - low];
+        }
     }
 
-    // transfering all elements from temporary to arr //
-    for (int i = low; i <= high; i++) {
-        arr[i] = temp[i - low];
+    void mergeSort(vector<int> &arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int mid = (low + high) / 2 ;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);  // merging sorted halves
     }
-}
-
-void mergeSort(vector<int> &arr, int low, int high) {
-    if (low >= high) {
-        return;
-    }
-    int mid = (low + high) / 2 ;
-    mergeSort(arr, low, mid);
-    mergeSort(arr, mid + 1, high);
-    merge(arr, low, mid, high);  // merging sorted halves
-}
-
-int main() {
-    vector<int> arr = {9, 4, 7, 6, 3, 1, 5}  ;
-    int n = 7;
-
-    cout << "Before Sorting Array: " << endl;
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " "  ;
-    }
-    cout << endl;
-    mergeSort(arr, 0, n - 1);
-    cout << "After Sorting Array: " << endl;
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " "  ;
-    }
-    cout << endl;
-    return 0 ;
-}
-
-// Before Sorting Array: 
-// 9 4 7 6 3 1 5 
-// After Sorting Array: 
-// 1 3 4 5 6 7 9 
+};
