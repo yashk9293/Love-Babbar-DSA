ad14b39f86571e48a8e6c9958a4087cc8a464fd9
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2025-03-06 02:01:16 +0530

    Update 83_0.cpp

--- Lec_083/83_0.cpp
+++ Lec_083/83_0.cpp
@@ -1,57 +1,61 @@
 // Question Link :- https://leetcode.com/problems/n-queens/
-// N-Queens (Striver)
+// N-Queens
 
-// Approach - 1
+
 // T.C = O(N! * N)
+// The first queen has n choices.
+// The second queen has at most (n-1) choices.
+// The third queen has at most (n-2) choices, and so on.
+
+// ..And additional O(N) factor due to safety checks
+
 // S.C = O(N^2)
 class Solution {
-private:
-    bool isSafe(int row, int col, vector<string> &board, int n) {
-        // check upper diagonal
-        int duprow = row;
-        int dupcol = col;
-
-        while(row>=0 && col>=0) {
-            if(board[row][col]= 'Q') {
+public:
+    bool isSafe(int row, int col, vector<string> &grid, int n) {
+        // check left row
+        for(int i=0; i<col; i++){
+            if(grid[row][i] == 'Q'){
                 return false;
-                row--;
-                col--
             }
         }
+        // check upper left diagonal
+        int i = row;
+        int j = col;
 
-        row=duprow;
-        col=dupcol;
-        while(col>=0) {
-            if(board[row][col]='Q') {
+        while(i>=0 && j>=0) {
+            if(grid[i][j]  == 'Q') {
                 return false;
             }
-            col--;
+            i--;
+            j--;
         }
 
-        row=duprow;
-        col=dupcol;
-        while(row<n && col>=0) {
-            if(board[row][col]= 'Q') {
+        // check lower left diagonal
+        i = row;
+        j = col;
+        while(i<n && j>=0) {
+            if(grid[i][j] == 'Q') {
                 return false;
-                row++;
-                col--
             }
+            i++;
+            j--;
         }
 
         return true;
     }
 
-public:
-    void solve(int col, vector<string> &board, vector<vector<string>> &ans, int n) {
+    void solve(int col, vector<string> &grid, vector<vector<string>> &ans, int n) {
         if(col == n) {
-            ans.push_back(board);
+            ans.push_back(grid);
             return;
         }
+
         for(int row=0; row<n; row++) {
-            if(isSafe(row, column, board, n)) {
-                board[row][column]='Q';
-                solve(col+1, board, ans, n);
-                board[row][column]='.';
+            if(isSafe(row, col, grid, n)) {
+                grid[row][col]='Q';
+                solve(col+1, grid, ans, n);
+                grid[row][col]='.';
             }
         }
     }
@@ -59,11 +63,7 @@ public:
 public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        vector<string> board(n);
-        string s(n, ".");
-        for(int i=0; i<n; i++) {
-            board[i]=s;
-        }
+        vector<string> board(n, string(n, '.')); // here it will be '.' not "." because the second parameter of the std::string constructor expects a char
         solve(0, board, ans, n);
         return ans;
     }
